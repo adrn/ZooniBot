31e570c1510fa264c6c96793c4370a93a4756d26
Adrian Price-Whelan <adrian.prw@gmail.com>
2012-07-10 11:47:04 +0200

    skeleton code

--- zoonibot/bot.py
+++ zoonibot/bot.py
@@ -5,7 +5,12 @@
 # Standard Library
 import os,sys
 import urllib, urllib2
-
+try:
+    # simplejson *is* json, but this works for earlier versions of Python
+    import simplejson as json
+except ImportError: 
+    import json
+    
 class Bot(object):
     pass
     
