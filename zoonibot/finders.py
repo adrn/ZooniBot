af351a87221872622a998e0b08a0ff13ecd23ffd
Adrian Price-Whelan <adrian.prw@gmail.com>
2012-07-10 21:48:45 +0200

    add docstrings and update documentation. also added TODOs

--- zoonibot/finders.py
+++ zoonibot/finders.py
@@ -1,2 +1,4 @@
+""" TODO: Describe this module! """
+
 def find_help_tags(bot):
     return bot.search_comments(['help'])
--- zoonibot/responses.py
+++ zoonibot/responses.py
@@ -1,30 +1,30 @@
-import entry
+""" TODO: Describe this module! """
+
+# Standard Library
 import copy
+
+# Project
+import entry
 
 def response_packer(old_comment, response):
     result = copy.deepcopy(old_comment)
     result.comment = response
     return result
 
-def yourmom_response(zoocomment):
-    """Returns a ZooniverseComment object containing a canned response
-    to an input ZooniverseComment object.
+def yourmom_response(zooniverse_comment):
+    """ Returns a ZooniverseComment object containing a canned response
+        to an input ZooniverseComment object.
     """
 
-    separator = ',' + ' '
-    tag_list_str = separator.join(zoocomment.comment.tags)
-    response_body = (
-            "Uh, your mom posted about {0}.".format(tag_list_str))
+    tag_list_str = ", ".join(zooniverse_comment.comment.tags)
+    response_comment = entry.Comment(body="Uh, your mom posted about {0}.".format(tag_list_str))
+    
+    return response_packer(zooniverse_comment, response_comment)
 
-    # Make a deep copy of the input ZooniverseComment.  Modify only
-    # the comment.body and author fields.
-    zooresponse = copy.deepcopy(zoocomment)
-    zooresponse.comment.body = response_body
-    zooresponse.author = ''
-
-    return zooresponse
-
-def help_response(zoocomment):
-
-    result = entry.Comment(body="'Hiya Have you tried our [tutorial video](http://www.planethunters.org/site_guide#video "") and [site guide](http://www.planethunters.org/site_guide "") - they are both good resources for new people")
-    return response_packer(zoocomment, result)
+def help_response(zooniverse_comment):
+    """ Returns a ZooniverseComment object with the canned #help response
+        from Meg Schwamb (e.g. https://github.com/adrn/ZooniBot/wiki/responses-and-tags)
+    """
+    
+    response_comment = entry.Comment(body="Hiya Have you tried our [tutorial video](http://www.planethunters.org/site_guide#video "") and [site guide](http://www.planethunters.org/site_guide "") - they are both good resources for new people")
+    return response_packer(zooniverse_comment, response_comment)
