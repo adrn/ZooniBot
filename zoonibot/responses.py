9fbb6bf5ac0dd6b172d1f7dc614937739560f422
Gabe Perez-Giz <gabe.perezgiz@nyu.edu>
2012-07-10 11:31:00 -0400

    Added responses.py file. Currently only has a yourmom response.

---
+++ zoonibot/responses.py
@@ -0,0 +1,20 @@
+import entry
+import copy
+
+def yourmom_response(zoocomment):
+    """Returns a ZooniverseComment object containing a canned response
+    to an input ZooniverseComment object.
+    """
+
+    separator = ',' + ' '
+    tag_list_str = separator.join(zoocomment.comment.tags)
+    response_body = (
+            "Uh, your mom posted about {0}.".format(tag_list_str))
+
+    # Make a deep copy of the input ZooniverseComment.  Modify only
+    # the comment.body and author fields.
+    zooresponse = copy.deepcopy(zoocomment)
+    zooresponse.comment.body = response_body
+    zooresponse.author = ''
+
+    return zooresponse
