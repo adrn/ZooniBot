94bc49390192e45083435412f7842e6a7bd486b9
Adrian Price-Whelan <adrian.prw@gmail.com>
2012-07-10 12:11:03 +0200

    add test and more skeleton code

--- zoonibot/bot.py
+++ zoonibot/bot.py
@@ -16,3 +16,15 @@ class Bot(object):
     
 class CommentBot(Bot):
     pass
+    
+class ZooniBot(CommentBot):
+    """ The ZooniBot is an automated robot for commenting on
+        Zooniverse objects.
+    """
+    
+    def __init__(self, api_key):
+        self.api_key = api_key
+    
+    def post(comment):
+        """ Post the comment to the Zooniverse by zoonibot """
+        pass
---
+++ zoonibot/test_bot.py
@@ -0,0 +1,8 @@
+import bot
+
+def test_zoonibot():
+    api_key = "d1b5be9242fb65de9372"
+    zoonibot = bot.ZooniBot(api_key)
+    
+    
+
