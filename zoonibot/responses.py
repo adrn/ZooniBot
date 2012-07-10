177bc1b184cef8e9be6e2ceb2a3af51a8e548c27
Adrian Price-Whelan <adrian.prw@gmail.com>
2012-07-10 22:47:39 +0200

    fix typo in function def

--- zoonibot/responses.py
+++ zoonibot/responses.py
@@ -25,10 +25,10 @@ def help_response(zooniverse_comment):
     response_comment = entry.Comment(body="""Hiya Have you tried our [tutorial video](http://www.planethunters.org/site_guide#video "") and [site guide](http://www.planethunters.org/site_guide "") - they are both good resources for new people #zoonibotans""")
     return response_packer(zooniverse_comment, response_comment)
 
-def planet_binaries_resposne(zooniverse_comment):
+def planet_binaries_response(zooniverse_comment):
     """ Canned response to handle cases where a light curve is flagged as both
         a planet *and* an eclipsing binary or a transit *and* an eclipsing binary.
-    """
+    """ 
     response_comment = entry.Comment(body="You said there was a planet transit but this is a known #eclipsingbinary where there is a star moving in front of another star. #zoonibotans")
     return response_packer(zooniverse_comment, response_comment)
     
